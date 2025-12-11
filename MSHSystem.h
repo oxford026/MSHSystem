1aad9121f8a84500e138a6c034c6250dc7abe85e
Mahmoud Jalloh <mahmoudjalloh63@gmail.com>
2025-12-11 22:40:54 +0300

    Add SystemMode and SystemState fields to MSHSystem (LLR23 prep)

--- MSHSystem.h
+++ MSHSystem.h
@@ -1,11 +1,33 @@
 #pragma once
 
+#include <string>
+
+enum class SystemMode
+{
+    Normal,
+    Evening,
+    Party,
+    Cinema
+};
+
+enum class SystemState
+{
+    Normal,
+    Sleep,
+    HighPerformance
+};
+
 class MSHSystem
 {
 public:
+    MSHSystem();
     void run();
 
 private:
     void displayMainMenu();
     void handleUserSelection(int selection);
+    void showHomeStatus() const; // Option 1
+
+    SystemMode currentMode;
+    SystemState currentState;
 };
